e8667059a328dbe7b43bdd87487c93980b5345d1
demingongo <sdemingongo@gmail.com>
2026-07-19 17:07:22 +0200

    fix calculateJwkThumbprint

--- packages/oauth2-jwt/scripts/build_npm.ts
+++ packages/oauth2-jwt/scripts/build_npm.ts
@@ -14,7 +14,7 @@ await build({
   shims: {},
   package: {
     name: "@saurbit/oauth2-jwt",
-    version: "0.1.6",
+    version: "0.1.7",
     description: "JWT utilities for @saurbit/oauth2 (jose-based)",
     license: "MIT",
     repository: {
